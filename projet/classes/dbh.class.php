7c2e08ac9b775e866b770aa4310727cbe1c66de3
kordo08 <ib_frioui@esi.dz>
2020-05-19 12:09:03 +0100

    Final commit

--- projet/classes/dbh.class.php
+++ projet/classes/dbh.class.php
@@ -10,7 +10,7 @@
   protected function connect()
   {
     try{
-        $dsn = "pgsql:host=" . $this->host . ";port=" . $this->port .";dbname=" . $this->dbname . ";user=" . $this->user . ";password=" . $this->password . ";" ;
+        $dsn = "pgsql:host=" . $this->host . " port=" . $this->port ." dbname=" . $this->dbname . " user=" . $this->user . " password=" . $this->password ;
         $pdo = new PDO($dsn, $this->user, $this->password);
         $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_OBJ);
         $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES,false);
