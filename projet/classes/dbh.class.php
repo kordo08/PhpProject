35af27669e26ef6712220730e48f3156866756ff
kordo08 <ib_frioui@esi.dz>
2020-05-19 11:41:06 +0100

    Final commit

--- projet/classes/dbh.class.php
+++ projet/classes/dbh.class.php
@@ -1,21 +1,24 @@
 <?php
  
  class dbh{
-  private $host="localhost";
-  private $user="root";
-  private $password="";
-  private $dbName="projet";
+  private $host="ec2-34-200-72-77.compute-1.amazonaws.com";
+  private $user="lbwymdnqknqhau";
+  private $password="88431a880f5b3fe88f9d071d0c72d335628cd8a25331af024cea065029d6acf9";
+  private $dbName="d6bplr9hrnmj0j";
+  private $port = "5432";
+
   protected function connect()
   {
-  try{
-  $pdo= new PDO("mysql::host=$this->host;dbname=$this->dbName",$this->user,$this->password);
-  $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_ASSOC);
-  }
-  catch(PDOException $e) {
-   echo "oooops sorry something went wrong with the connection :( <br>".$e->getMessage();
-  }
-  return $pdo;
-  }
- } 
- 
-?> 
+    try{
+        $dsn = "pgsql:host=" . $this->host . ";port=" . $this->port .";dbname=" . $this->dbname . ";user=" . $this->user . ";password=" . $this->password . ";" ;
+        $pdo = new PDO($dsn, $this->user, $this->password);
+        $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_OBJ);
+        $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES,false);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+      }
+      catch (PDOException $e) {
+     echo "oooops sorry something went wrong with the connection :( <br>".$e->getMessage();
+      }
+    }
+}
+        ?>
