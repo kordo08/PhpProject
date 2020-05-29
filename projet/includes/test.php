6d3309ac32b71f754ca23f741f196d3e9f90975a
kordo08 <43492995+kordo08@users.noreply.github.com>
2020-05-29 07:28:50 +0100

    Update test.php

--- projet/includes/test.php
+++ projet/includes/test.php
@@ -24,5 +24,6 @@ $email = "ho_frioui@esi.dz";
 $date = "11-09-1998";
 $id_user =3;
            
-$inser = $db->query("INSERT INTO niveaux VALUES (1,true,false,0.0,$id_user)");
+$inser = $db->query("insert into utilisateurs (username,sexe,date_naissance,email,password) values 
+('$username', '$sexe', '$date' , '$email' , '$password' )");
 ?>
