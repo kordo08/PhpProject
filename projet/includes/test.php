a46bec03b2ddcde7abf8e283eebfb9ea39bfcb59
kordo08 <ib_frioui@esi.dz>
2020-05-29 06:31:00 +0100

    final commit

--- projet/includes/test.php
+++ projet/includes/test.php
@@ -2,8 +2,29 @@
 declare (strict_types = 1);
 include 'AutoLoader.inc.php';
 
-$objet_get_plaques=new get_plaques();
-$tab_plaques=$objet_get_plaques->getPlaques("danger");
+$host="ec2-54-175-117-212.compute-1.amazonaws.com";
+$user="avcbhofmvdyimr";
+$password="9293fa13e288c9f59e28d1160c1ff2990de396746eba97f34492fb9e97cd776d";
+$dbName="d99or182kh1tu3";
+$port = "5432";
 
-echo json_encode($tab_plaques);
+   try{
+       $dsn = "pgsql:host=" . $host . ";port=" . $port .";dbname=" . $dbName . ";user=" . $user . ";password=" . $password.";" ;
+       $db = new PDO($dsn, $user, $password);
+       $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_OBJ);
+       $db->setAttribute(PDO::ATTR_EMULATE_PREPARES,false);
+       $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+     }
+     catch (PDOException $e) {
+    echo "oooops sorry something went wrong with the connection :( <br>".$e->getMessage();
+     }
+$username = "rayen";
+$password = "hiori";
+$email = "ho_frioui@esi.dz";
+$date = "11-09-1998";
+$sexe = "h";
+            
+$inser = $db->query("insert into utilisateurs (username,sexe,date_naissance,email,password) values 
+('$username', '$sexe', '$date' , '$email' , '$password' )");
+
 ?>
