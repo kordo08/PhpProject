d34cc01a445ab9e1925502b6d65f189a63360f7b
kordo08 <ib_frioui@esi.dz>
2020-05-29 03:16:18 +0100

    final commit

--- projet/includes/test.php
+++ projet/includes/test.php
@@ -5,5 +5,5 @@ include 'AutoLoader.inc.php';
 $objet_get_plaques=new get_plaques();
 $tab_plaques=$objet_get_plaques->getPlaques("danger");
 
-echo "json_encode($tab_plaques)";
+echo json_encode($tab_plaques);
 ?>
