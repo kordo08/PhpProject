a9a603c732f30877b0007b7c0489644f4da4eda3
kordo08 <43492995+kordo08@users.noreply.github.com>
2020-06-11 20:21:37 +0100

    Update test.php

--- projet/includes/test.php
+++ projet/includes/test.php
@@ -1,1 +1,23 @@
 
+ <?php
+
+$host="ec2-54-175-117-212.compute-1.amazonaws.com";
+$user="avcbhofmvdyimr";
+$password="9293fa13e288c9f59e28d1160c1ff2990de396746eba97f34492fb9e97cd776d";
+$dbName="d99or182kh1tu3";
+$port = "5432";
+
+   try{
+       $dsn = "pgsql:host=" . $host . ";port=" . $port .";dbname=" . $dbName . ";user=" . $user . ";password=" . $password.";" ;
+       $db = new PDO($dsn, $user, $password);
+       $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_OBJ);
+       $db->setAttribute(PDO::ATTR_EMULATE_PREPARES,false);
+       $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+     }
+     catch (PDOException $e) {
+    echo "oooops sorry something went wrong with the connection :( <br>".$e->getMessage();
+     }
+$req1=$db->query("INSERT INTO niveaux VALUES ('' ,1,true,false,0.0,1),
+                                               ('' ,2,false,false,0.0,1),
+                                              ('' ,3,false,false,0.0,1);");
+?>
